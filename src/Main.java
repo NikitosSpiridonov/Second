61804dfef05837e58090b95691827f5d0c2d57ee
Nikita Spiridonov <spiridonov.n.i@yandex.ru>
2024-02-01 00:26:17 +0300

    second

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        double Price = 13676; // цена билета
+        int Miles = (int) (Price / 20); // количество начисленных миль
+
+
+        System.out.println("Начисленные мили: " + Miles);
+    }
+}
